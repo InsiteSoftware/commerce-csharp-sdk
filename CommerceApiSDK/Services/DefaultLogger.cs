6182f59a688d143fc6e36952435db5b6c31cd720
Bhavani Annadurai <bhavani.annadurai@perficient.com>
2022-02-11 13:20:30 +0530

    Abstract Logging files added - MAS29

---
+++ CommerceApiSDK/Services/DefaultLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommerceApiSDK.Services.Interfaces;
+
+namespace CommerceApiSDK.Services
+{
+    public class DefaultLogger : ILoggerService
+    {
+        public void LogConsole(LogLevel level, string message)
+        {
+            string line = $"[{level}] : {message}";
+            Console.WriteLine(line);
+        }
+
+        public void LogDebug(LogLevel level, string message)
+        {
+            string line = $"[{level}] : {message}";
+            System.Diagnostics.Debug.WriteLine(line);
+        }
+    }
+}
