1d5202f0c0412dea6a892f7be9270727cc44d1e8
Waseem Pasha <waseem.pasha@perficient.com>
2022-03-02 12:53:05 +0530

    MAS-66 Tokenexconfig endpoint added.

---
+++ CommerceApiSDK/Services/Interfaces/ITokenexconfigService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommerceApiSDK.Models;
+using CommerceApiSDK.Models.Results;
+
+namespace CommerceApiSDK.Services.Interfaces
+{
+    public interface ITokenexconfigService
+    {
+        Task<TokenExDto> GetTokenexconfigAsync();
+    }
+}
---
+++ CommerceApiSDK/Services/TokenexconfigService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommerceApiSDK.Models;
+using CommerceApiSDK.Services.Interfaces;
+
+namespace CommerceApiSDK.Services
+{
+    public class TokenexconfigService : ServiceBase, ITokenexconfigService
+    {
+        private const string TokenexconfigUrl = "/api/v1/tokenexconfig";
+
+        public TokenexconfigService(IClientService clientService, INetworkService networkService, ITrackingService trackingService, ICacheService cacheService, ILoggerService loggerService)
+             : base(clientService, networkService, trackingService, cacheService, loggerService)
+        {
+        }
+
+        public async Task<TokenExDto> GetTokenexconfigAsync()
+        {
+            try
+            {
+                TokenExDto tokenexConfig= await GetAsyncNoCache<TokenExDto>(TokenexconfigUrl, DefaultRequestTimeout);
+
+                if (tokenexConfig == null)
+                {
+                    return null;
+                }
+
+                return tokenexConfig;
+            }
+            catch (Exception exception)
+            {
+                TrackingService.TrackException(exception);
+                return null;
+            }
+        }
+    }
+}
