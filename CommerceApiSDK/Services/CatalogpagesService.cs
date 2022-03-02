294e5c565d27eca368d2995a06eee5cc9d1c614b
BhavaniAnnadurai <98167050+BhavaniAnnadurai@users.noreply.github.com>
2022-03-02 11:34:55 +0530

    MAS-64 Catalogpages endpoint added. (#11)

---
+++ CommerceApiSDK/Services/CatalogpagesService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommerceApiSDK.Models.Results;
+using CommerceApiSDK.Services.Interfaces;
+
+namespace CommerceApiSDK.Services
+{
+    public class CatalogpagesService : ServiceBase, ICatalogpagesService
+    {
+        private const string CatalogpageUrl = "/api/v1/catalogpages?path=";
+
+        public CatalogpagesService(IClientService clientService, INetworkService networkService, ITrackingService trackingService, ICacheService cacheService, ILoggerService loggerService)
+            : base(clientService, networkService, trackingService, cacheService, loggerService)
+        {
+        }
+
+        public async Task<CatalogpagesResult> GetProductCatalogInformation(string productPath)
+        {
+            try
+            {
+                string url = $"{CatalogpageUrl}{productPath}";
+
+                CatalogpagesResult productResult = await GetAsyncWithCachedResponse<CatalogpagesResult>(url);
+
+                if (productResult == null)
+                {
+                    return null;
+                }
+
+                return productResult;
+            }
+            catch (Exception exception)
+            {
+                TrackingService.TrackException(exception);
+                return null;
+            }
+        }
+    }
+}
