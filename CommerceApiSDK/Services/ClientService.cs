0e7c5ea577888812bc5b6b8cee60799743350098
naveenperficient <98829210+naveenperficient@users.noreply.github.com>
2022-03-21 08:31:45 +0530

    MAS-97 Created new directory Handler, Moved RefreshTokenHandler class into this folder
    Made httpClientHandler as public

--- CommerceApiSDK/Services/ClientService.cs
+++ CommerceApiSDK/Services/ClientService.cs
@@ -7,6 +7,7 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using CommerceApiSDK.Extensions;
+using CommerceApiSDK.Handler;
 using CommerceApiSDK.Models;
 using CommerceApiSDK.Models.Enums;
 using CommerceApiSDK.Models.Results;
@@ -33,7 +34,7 @@ namespace CommerceApiSDK.Services
         protected virtual string CookiesStorageKey { get; } = "cookies";
 
         protected HttpClient client;
-        private HttpClientHandler httpClientHandler;
+        public HttpClientHandler httpClientHandler;
 
         private readonly ISecureStorageService secureStorageService;
         private readonly ILocalStorageService localStorageService;
@@ -411,86 +412,7 @@ namespace CommerceApiSDK.Services
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
-        private class RefreshTokenHandler : DelegatingHandler
-        {
-            private readonly Func<Task<bool>> renewAuthenticationTokensCallback;
-            private readonly object refreshingTokenLock = new object();
-
-            private readonly Action refreshTokenExpiredNotificationCallback;
-            private readonly ILoggerService loggerService;
-
-            public RefreshTokenHandler(
-                HttpMessageHandler messageHandler,
-                Func<Task<bool>> renewAuthenticationTokensCallback, ILoggerService loggerService,
-                Action refreshTokenExpiredNotificationCallback) : base(messageHandler)
-            {
-                this.renewAuthenticationTokensCallback = renewAuthenticationTokensCallback;
-                this.refreshTokenExpiredNotificationCallback = refreshTokenExpiredNotificationCallback;
-                this.loggerService = loggerService;
-            }
-
-            private CancellationTokenSource GetCancellationTokenSource(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                TimeSpan timeout = request.GetTimeout();
-                if (timeout == Timeout.InfiniteTimeSpan)
-                {
-                    // No need to create a CTS if there's no timeout
-                    return null;
-                }
-                else
-                {
-                    CancellationTokenSource cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-                    cts.CancelAfter(timeout);
-                    return cts;
-                }
-            }
-
-            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                loggerService.LogConsole(LogLevel.INFO, "Sending Async request : {0} {1}", request, cancellationToken);
-                HttpResponseMessage result = null;
-                
-                    using (CancellationTokenSource cts = GetCancellationTokenSource(request, cancellationToken))
-                    {
-                        try
-                        {
-                            result = await base.SendAsync(request, cts?.Token ?? cancellationToken);
-                            loggerService.LogConsole(LogLevel.INFO, "SendAsync Response : {0}", result);
-                        }
-                        catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested)
-                        {
-                            throw new TimeoutException();
-                        }
-                    }
-
-                    if (result.IsSuccessStatusCode)
-                    {
-                        return result;
-                    }
-
-                    if (request.RequestUri.AbsolutePath.Contains(CommerceAPIConstants.TokenUri) || request.RequestUri.AbsolutePath.Contains(CommerceAPIConstants.TokenLogoutUri))
-                    {
-                        return result;
-                    }
-
-                    if (result.StatusCode == HttpStatusCode.Unauthorized || result.StatusCode == HttpStatusCode.Forbidden)
-                    {
-                        await Task.Run(() =>
-                        {
-                            lock (refreshingTokenLock)
-                            {
-                                bool success = renewAuthenticationTokensCallback().Result;
-                                if (!success)
-                                {
-                                    refreshTokenExpiredNotificationCallback?.Invoke();
-                                }
-                            }
-                        });
-                    }
-
-                return result;
-            }
-        }
+      
 
         #region Access Token
 
