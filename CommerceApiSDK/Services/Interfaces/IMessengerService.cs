486da3a0fefd86c472d9fbee632d2e0395d6cedf
Saiful Islam <saiful.islam@optimizely.com>
2022-10-12 21:32:56 +0600

    ICM-3438 Existing Subscribe guid not holding onto the action which is not getting called when publish is getting called. Took inspiration MvvmCross messenger plugin for this.

--- CommerceApiSDK/Services/Interfaces/IMessengerService.cs
+++ CommerceApiSDK/Services/Interfaces/IMessengerService.cs
@@ -5,6 +5,8 @@ namespace CommerceApiSDK.Services.Interfaces
 {
     public interface IMessengerService
     {
+        OptiSubscriptionToken OnSubscribe<TMessage>(Action<TMessage> action) where TMessage : OptiMessage;
+
         Guid Subscribe<TMessage>(Action<TMessage> action) where TMessage : OptiMessage;
 
         void Unsubscribe<TMessage>(Guid subscriptionId) where TMessage : OptiMessage;
--- CommerceApiSDK/Services/MessengerService.cs
+++ CommerceApiSDK/Services/MessengerService.cs
@@ -5,6 +5,37 @@ using CommerceApiSDK.Services.Messages;
 
 namespace CommerceApiSDK.Services
 {
+    public sealed class OptiSubscriptionToken
+    : IDisposable
+    {
+        public Guid Id { get; private set; }
+        #pragma warning disable 414 // 414 is that this private field is only set, not used
+        private readonly object[] _dependentObjects;
+        #pragma warning restore 414
+        private readonly Action _disposeMe;
+
+        public OptiSubscriptionToken(Guid id, Action disposeMe, params object[] dependentObjects)
+        {
+            Id = id;
+            _disposeMe = disposeMe;
+            _dependentObjects = dependentObjects;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                _disposeMe();
+            }
+        }
+    }
+
     public class MessengerService : IMessengerService
     {
         private static readonly ConcurrentDictionary<
@@ -14,6 +45,15 @@ namespace CommerceApiSDK.Services
             Type,
             ConcurrentDictionary<Guid, BaseSubscription>
         >();
+
+        public OptiSubscriptionToken OnSubscribe<TMessage>(Action<TMessage> action) where TMessage : OptiMessage
+        {
+            var subscriptionId = Subscribe(action);
+            return new OptiSubscriptionToken(
+                    subscriptionId,
+                    () => Unsubscribe<TMessage>(subscriptionId),
+                    action);
+        }
 
         public Guid Subscribe<TMessage>(Action<TMessage> action) where TMessage : OptiMessage
         {
